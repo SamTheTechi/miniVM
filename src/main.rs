cde320a92b2b929821828184baffcd3ed328a2ab
SamTheProBot <gsameer478@gmail.com>
2025-01-21 04:21:26 +0530

    basic instruction set done

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 mod vm;
-
 use vm::VM;
 
 fn main() {
-    println!("Hello, world!");
+    let mut vm = VM::new();
+    let program: Vec<i32> = vec![
+        0x00000008, // push 8
+        0x00000005, // push 5
+        0x00000002, // push 2
+        0x40000002, // add
+        0x00000003, // push 3
+        0x40000004, // mul
+        0x00000007, // push 7
+        0x40000005, // div
+        0x40000003, // sub
+        0x40000001, // halt
+    ];
+    vm.load_program(program);
+    vm.run();
 }
--- src/vm.rs
+++ src/vm.rs
@@ -8,37 +8,119 @@ pub struct VM {
 }
 
 impl VM {
-    fn _fatch(&mut self) {
-        self.pc = self.pc + 1;
-    }
-    fn _decode(&mut self) {
-        self.typ = Self::get_type(self.memory[self.pc]);
-        self.dat = Self::get_data(self.memory[self.pc]);
-    }
-    fn _execute(&mut self) {
-        if self.typ == 0 || self.typ == 2 {
-            self.sp = self.sp + 1;
-        } else {
-            Self::additional();
+    pub fn new() -> Self {
+        VM {
+            pc: 100,
+            sp: 0,
+            memory: vec![0; 1_000_000],
+            typ: 0,
+            dat: 0,
+            running: 1,
         }
     }
 
     fn get_type(instruction: i32) -> i32 {
-        let mut val: i32 = 0xc0000000;
+        let mut val: i32 = 0xc0000000u32 as i32;
         val = (val & instruction) >> 30;
         val
     }
+
     fn get_data(instruction: i32) -> i32 {
         let mut val: i32 = 0x3fffffff;
         val = val & instruction;
         val
     }
-    fn additional() {}
 
-    pub fn _run() {
-        println!("starting the VM !");
+    fn fatch(&mut self) {
+        self.pc = self.pc + 1;
     }
-    pub fn _load_program() {
-        println!("starting the VM !");
+    fn decode(&mut self) {
+        self.typ = Self::get_type(self.memory[self.pc as usize]);
+        self.dat = Self::get_data(self.memory[self.pc as usize]);
+    }
+    fn execute(&mut self) {
+        if self.typ == 0 || self.typ == 2 {
+            self.sp += 1;
+            self.memory[self.sp as usize] = self.dat;
+            println!("push: {}", self.memory[self.sp as usize])
+        } else {
+            self.instruction_set();
+        }
+    }
+
+    fn instruction_set(&mut self) {
+        match self.dat {
+            0 => {
+                // nop
+                println!("nop instruction");
+            }
+            1 => {
+                // hlt
+                println!("hlt");
+                self.running = 0;
+            }
+            2 => {
+                // add
+                println!(
+                    "add {} + {}",
+                    self.memory[(self.sp - 1) as usize],
+                    self.memory[self.sp as usize]
+                );
+                self.memory[self.sp as usize - 1] += self.memory[self.sp as usize];
+                self.sp -= 1;
+                println!("tos: {}", self.memory[self.sp as usize]);
+            }
+            3 => {
+                // sub
+                println!(
+                    "sub {} - {}",
+                    self.memory[(self.sp - 1) as usize],
+                    self.memory[self.sp as usize]
+                );
+                self.memory[self.sp as usize - 1] -= self.memory[self.sp as usize];
+                self.sp -= 1;
+                println!("tos: {}", self.memory[self.sp as usize]);
+            }
+            4 => {
+                // mul
+                println!(
+                    "mul {} * {}",
+                    self.memory[(self.sp - 1) as usize],
+                    self.memory[self.sp as usize]
+                );
+                self.memory[self.sp as usize - 1] *= self.memory[self.sp as usize];
+                self.sp -= 1;
+                println!("tos: {}", self.memory[self.sp as usize]);
+            }
+            5 => {
+                // div
+                println!(
+                    "div {} / {}",
+                    self.memory[(self.sp - 1) as usize],
+                    self.memory[self.sp as usize]
+                );
+                self.memory[self.sp as usize - 1] /= self.memory[self.sp as usize];
+                self.sp -= 1;
+                println!("tos: {}", self.memory[self.sp as usize]);
+            }
+            _ => {
+                println!("unknown instruction");
+            }
+        }
+    }
+
+    pub fn run(&mut self) {
+        self.pc -= 1;
+        while self.running == 1 {
+            self.fatch();
+            self.decode();
+            self.execute();
+        }
+    }
+
+    pub fn load_program(&mut self, prog: Vec<i32>) {
+        for i in 0..prog.len() {
+            self.memory[(self.pc as usize + i) as usize] = prog[i];
+        }
     }
 }
